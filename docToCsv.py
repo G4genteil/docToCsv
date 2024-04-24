af0d47e0acebd035733efb1f88a589acdb0b49e4
Daniel Mensinger <daniel@mensinger-ka.de>
2024-04-24 21:46:27 +0200

    Quick refactor

--- docToCsv.py
+++ docToCsv.py
@@ -1,17 +1,50 @@
 #!/usr/bin/env python
 
-import easyocr
 # from pdf2image import convert_from_path
 import fitz
 from pathlib import Path
 import argparse as AP
 import shutil
 from PIL import Image
+import json
+
+from pprint import pprint
 
 ROOT = Path(__file__).parent.resolve()
+TMP_OUT = ROOT / 'tmp'
+
+def extract_images(in_file: Path) -> None:
+    if TMP_OUT.exists():
+        shutil.rmtree(TMP_OUT)
+
+    TMP_OUT.mkdir(parents=True)
+
+    pdf = fitz.open(in_file)
+    for page in pdf:
+        for img in page.get_images():
+            img_data = pdf.extract_image(img[0])
+
+            img_out = TMP_OUT / f'{img[0]}.{img_data["ext"]}'
+            img_out.write_bytes(img_data['image'])
+
+            tmp_img = Image.open(img_out)
+            tmp_img = tmp_img.rotate(-90, expand=True)
+            tmp_img.save(img_out)
+
+def read_images() -> list[list]:
+    import easyocr
+    reader = easyocr.Reader(['en', 'de']) # this needs to run only once to load the model into memory
+
+    res = []
+    for img in TMP_OUT.glob("*"):
+        res += [reader.readtext(str(img))]
+
+    return res
+
+def gen_csv(pages: list[list]) -> None:
+    pprint(pages)
 
 def main() -> int:
-
     parser = AP.ArgumentParser('Doc To CSV')
     parser.add_argument('input', type=Path, help='Pfad zur input PDF Datei')
 
@@ -19,34 +52,11 @@ def main() -> int:
 
     in_file: Path = args.input
 
-    tmp_out = ROOT / 'tmp'
-    if tmp_out.exists():
-        shutil.rmtree(tmp_out)
+    #extract_images(in_file)
+    #pages = read_images()
+    pages = [json.loads((ROOT / 'data.json').read_text())]
+    gen_csv(pages)
 
-    tmp_out.mkdir(parents=True)
-
-    pdf = fitz.open(in_file)
-    for page in pdf:
-        for img in page.get_images():
-            img_data = pdf.extract_image(img[0])
-
-            img_out = tmp_out / f'{img[0]}.{img_data["ext"]}'
-            img_out.write_bytes(img_data['image'])
-
-            tmp_img = Image.open(img_out)
-            tmp_img = tmp_img.rotate(-90, expand=True)
-            tmp_img.save(img_out)
-
-    reader = easyocr.Reader(['en', 'de']) # this needs to run only once to load the model into memory
-
-    for img in tmp_out.glob("*"):
-        print(img)
-        result = reader.readtext(str(img))
-        for i in result:
-            print(i)
-
-
-    return 0
 
 if __name__ == '__main__':
-    main()
+    raise SystemExit(main())
